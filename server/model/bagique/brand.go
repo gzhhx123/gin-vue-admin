ede4dee40c8cec46e1cf85290147f9f2a5e3eeee
wcq <742106460@qq.com>
2024-11-12 14:47:57 +0800

    1.完善品牌日期与logo 2.完善品牌详情 3.完善品牌日期筛选

--- server/model/bagique/brand.go
+++ server/model/bagique/brand.go
@@ -10,8 +10,8 @@ type Brand struct {
 	global.GVA_MODEL
 	BrandName      *string `json:"brandName" form:"brandName" gorm:"column:brand_name;comment:品牌名称;" binding:"required"` //品牌名称
 	BrandShortName *string `json:"brandShortName" form:"brandShortName" gorm:"column:brand_short_name;comment:品牌简称;"`    //品牌简称
-	BrandLogo      *string `json:"brandLogo" form:"brandLogo" gorm:"column:brand_logo;comment:品牌logo;"`                    //品牌logo
-	Remark         *string `json:"remark" form:"remark" gorm:"column:remark;comment:备注;"`                                  //备注
+	BrandLogo      *string `json:"brandLogo" form:"brandLogo" gorm:"column:brand_logo;comment:品牌logo;"`                  //品牌logo
+	Remark         *string `json:"remark" form:"remark" gorm:"column:remark;comment:备注;"`                                //备注
 }
 
 // TableName 品牌信息 Brand自定义表名 bagique_brands
